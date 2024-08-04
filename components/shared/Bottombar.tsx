b1ebaef889e17401f6131a8af5cba1b52411cbb3
Fran <fm-a@robindatahood.com>
2024-08-05 00:42:41 +0200

    1:00:00

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,7 +1,38 @@
+"use client"
+import { sidebarLinks } from '@/constants'
+import Link from 'next/link';
 import React from 'react'
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 export default function Bottombar() {
+ const pathname = usePathname()
   return (
-   <h1>Bottombar</h1>
+  <section className='bottombar'>
+    <div className='bottombar_container'>
+    {sidebarLinks.map((item) => {
+          const isActive =
+            (pathname.includes(item.route) && item.route.length > 1) ||
+            pathname === item.route;
+
+          return (
+            <Link
+              key={item.label}
+              href={item.route}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={item.imgURL}
+                alt={item.label}
+                width={24}
+                height={24}
+              />
+              <p className="text-light-1 text-subtle-medium  max-sm:hidden">
+                {item.label.split(/\s+/)[0]}</p>
+            </Link>
+          );
+        })}
+    </div>
+  </section>
   )
 }
