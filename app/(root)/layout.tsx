1cc492fefd1ff8d772105cbf9c730f6a1bbf072d
Fran <fm-a@robindatahood.com>
2024-08-05 00:25:00 +0200

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js 14 Meta Threads Application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+         <Topbar />
+         <main>
+          <LeftSidebar />
+          <section className="main-container">
+            <div className="w-full max-w-4xl">
+              {children}
+            </div>
+          </section>
+          <RightSidebar />
+         </main>
+        <Bottombar />
+        </body>
+     
+    </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+//36:56
+export default function Home() {
+  return (
+   <>
+    <h1 className="head-text text-left">Home</h1>
+   </>
+  );
+}
